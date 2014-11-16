96edc778b51a3d7370c2b9a4698ef9d4b34c994a
Jorge Colon - 2UP Media <info@2upmedia.com>
2014-11-16 04:44:47 -0500

    first commit

---
+++ tests/unit/EmailAddressValidatorTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace _2UpMedia;
+
+class EmailAddressValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $email
+     * @dataProvider getEmailAddresses
+     */
+    public function testAddUser($email)
+    {
+        $validator = new EmailAddressValidator($email, EmailAddressValidator::RFC_5322);
+
+        $this->assertTrue($validator->isValid());
+    }
+
+    public function testXssInjectedEmail()
+    {
+        $email = '<script>window.alert("hello");</script>hello@gmail.com';
+
+        $this->assertFalse(
+            (new EmailAddressValidator($email, EmailAddressValidator::RFC_5322))->isValid()
+        );
+    }
+
+    public function getEmailAddresses()
+    {
+        return array(
+            array('jorge@2upmedia.com'),
+            array('michael@squiloople.com'),
+            array('very.common@example.com'),
+            array('a.little.lengthy.but.fine@dept.example.com'),
+            array('disposable.style.email.with+symbol@example.com'),
+            array('user@[IPv6:2001:db8:1ff::a0b:dbd0]'),
+            array('"much.more unusual"@example.com'),
+            array('"very.unusual.@.unusual.com"@example.com'),
+            array('"very.(),:;<>[]\".VERY.\"very@\ \"very\".unusual"@strange.example.com'),
+            array('postbox@com'),
+            array('admin@mailserver1'),
+            array("!#$%&'*+-/=?^_`{}|~@example.org"),
+            array('"()<>[]:,;@\\"!#$%&\'*+-/=?^_`{}| ~.a"@example.org'),
+            array('" "@example.org'),
+//            array('üñîçøðé@example.com'),
+        );
+    }
+}
+ 
